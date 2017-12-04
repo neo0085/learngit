2af4eec6ddad49bed9a63da7edcef5deb5f7c6bf
ten <togsheng1010@gmail.com>
2017-12-04 20:30:37 +0800

    add python

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#-*-coding:utf-8-*-
+# A comment, this is so you can read your progran later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or commet out a piece of code:
+# print "This won't run."
+print "This will run."
+# #号有很多的英文名字，例如：octothorpe(八角帽），pound(英镑符），hash(电话的#键）
